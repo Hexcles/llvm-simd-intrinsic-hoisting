04010192cb53bad37db3fb65b32e283ca139778b
kathydxue <xuedongx@sfu.ca>
2016-03-24 17:13:20 -0700

    Added files via upload

--- test/avg.c
+++ test/avg.c
@@ -7,9 +7,7 @@ int main() {
 	a = _mm_set_epi16(1, 2, 3, 4, 5, 6, 7, 8);
 	b = _mm_set_epi16(5, 3, 2, 1, 4, 6, 1, 2);
 	c = _mm_avg_epu16(a, b);
-	/* __m128i is also an uint64_t[2]
-	 * Expected: 6 4
-	 */
+
 	printf("%llx %llx", c[1], c[0]);
 	return 0;
 }
--- test/sad.c
+++ test/sad.c
@@ -7,9 +7,7 @@ int main() {
 	a = _mm_set_epi16(1, 2, 3, 4, 5, 5, 6, 4);
 	b = _mm_set_epi16(5, 3, 2, 1, 4, 3, 6, 1);
 	c = _mm_sad_epu8(a, b);
-	/* __m128i is also an uint64_t[2]
-	 * Expected: 6 4
-	 */
+
 	printf("%llx %llx\n", c[1], c[0]);
 	return 0;
 }
